147af81fcf2cbc3b10058719fdf182e81c25f703
unknown <@IT.LOCAL>
2017-08-21 12:39:28 +0200

    Create parser class "ProxyList"

--- proxies_list.py
+++ proxies_list.py
@@ -2,29 +2,27 @@ import requests
 from lxml.html import fromstring
 
 
-url_site = 'https://exmo.me/ru/trade#?pair=BTC_USD'
-proxy_url = 'https://www.ip-adress.com/proxy_list/' #
+class ProxyList:
+    url_site = 'https://exmo.me/ru/trade#?pair=BTC_USD'
+    proxy_url = 'https://www.ip-adress.com/proxy_list/'
 
-def get_proxy_list(proxy_url):
-    str = fromstring((requests.get(proxy_url)).content)
-    ports = str.xpath("//table/tbody/tr/td[1]/text()")
-    ipaddresses = str.xpath("//table/tbody/tr/td[1]/a/text()")
-    proxy_list = [ipaddresses[i] + ports[i] for i in range(len(ipaddresses))]
-    return proxy_list
+    def __init__(self):
+        str = fromstring((requests.get(self.proxy_url)).content)
+        ports = str.xpath("//table/tbody/tr/td[1]/text()")
+        ipaddresses = str.xpath("//table/tbody/tr/td[1]/a/text()")
+        self.proxy_list = [ipaddresses[i] + ports[i] for i in range(len(ipaddresses))]
 
-def give_proxy(proxy_list, url_site):
-    for proxy in proxy_list:
-        url = 'http://' + proxy
-        try:
-            r = requests.get(url_site, proxies={'http':url})
-            if r.status_code == 200:
-                return url
-        except requests.exceptions.ConnectionError:
-            print('error')
-            continue
-
+    def get_proxy(self):
+        for proxy in self.proxy_list:
+            url = proxy
+            try:
+                r = requests.get(self.url_site, proxies={'http': url})
+                if r.status_code == 200:
+                    return url
+            except requests.exceptions.ConnectionError:
+                continue
 
 
-proxy_list = get_proxy_list(proxy_url)
-result = give_proxy(proxy_list, url_site)
-print(result)
+if __name__ == '__main__':
+    proxy = ProxyList()
+    print(proxy.get_proxy())
