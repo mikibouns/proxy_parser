441133ddd5b755dd8bf7231efee0bf2b1c55a3a9
unknown <@IT.LOCAL>
2017-09-01 15:04:26 +0200

    change variable

--- parse_proxy_requests.py
+++ parse_proxy_requests.py
@@ -6,20 +6,19 @@ from proxies_list import ProxyList # импортируем клас из лок
 
 class ParserProxy:
 
-    def __init__(self, proxy_list, currency_pair='ETH_USD'):
+    def __init__(self, proxy_list, currency_pair):
         self.currency_pair = currency_pair
         self.URL = 'https://exmo.me/ru/trade#?pair=BTC_USD' # ссылка на парсируемый сайт
-        self.proxy_list = proxy_list
         self.proxy = {
-            'http': self.proxy_list.get_proxy(),
+            'http': proxy_list.get_proxy(),
             }
-        print(self.proxy_list.get_proxy()) # выводим на экран результат работы метода get_proxy()
+        print(proxy_list.get_proxy()) # выводим на экран результат работы метода get_proxy()
 
     def get_rate(self):
         # получаем целевую страницу html используя прокси сервер
         parsed_body = html.fromstring(requests.get(self.URL, proxies=self.proxy).text)
         # получаем значение обходя дерево html через метод xpath
-        rate_pc = parsed_body.xpath("//div/ul/li[@pair='%s']/div[4]/text()" % self.currency_pair)
+        rate_cp = parsed_body.xpath("//div/ul/li[@pair='%s']/div[4]/text()" % self.currency_pair)
         return rate_cp
 
 
